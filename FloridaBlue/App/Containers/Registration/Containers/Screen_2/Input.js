81c3dea6ab484c663ad6308f9e986ec4751d4913
Enugu <f7ro@mac51b4660.bcbsfl.com>
2016-12-27 10:05:31 -0500

    test

--- FloridaBlue/App/Containers/Registration/Containers/Screen_2/Input.js
+++ FloridaBlue/App/Containers/Registration/Containers/Screen_2/Input.js
@@ -55,7 +55,7 @@ class Input extends Component{
 const Styles = StyleSheet.create({
 
   textInput:{
-    height: 31,
+    height: 30,
     borderBottomColor: '#000000',
     borderBottomWidth: 1,
     fontSize : 12,
