ae890676ceb62f06ce968c2e806fadf281aff4d4
Sean Wright <sean.wright@bcbsfl.com>
2017-01-26 15:53:58 -0500

    Implemented Login style changes

--- FloridaBlue/App/Themes/Images.js
+++ FloridaBlue/App/Themes/Images.js
@@ -3,12 +3,11 @@
 // leave off @2x/@3x
 const images = {
   logo: require('../Images/ir.png'),
-  clearLogo: require('../Images/top_logo.png'),
+  clearLogo: require('../Images/login-logo.png'),
   tileBg: require('../Images/tile_bg.png'),
   background: require('../Images/bkgrd-gradient.png'),
   regcompletehdr : require('../Images/reg-complete-hdr.png'),
-  loginIn : require('../Images/login-screen-login-btn-blue.png'),
-  background: require('../Images/bkgrd-gradient.png'),
+  loginButton : require('../Images/login-screen-login-btn-blue.png'),
   splashlog: require('../Images/splash-logo.png'),
   logInlog: require('../Images/login-logo.png'),
   clouds: require('../Images/clouds.png'),
