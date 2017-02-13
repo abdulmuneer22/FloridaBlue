092639b05307347fe01729f770acebd4324bd1c3
Prathipati <f7ih@mac51b5490.bcbsfl.com>
2017-02-13 11:28:21 -0500

    Plan benefit services changes

--- FloridaBlue/App/Containers/MemberDashBoard/Containers/DoctorService/Components/Card.js
+++ FloridaBlue/App/Containers/MemberDashBoard/Containers/DoctorService/Components/Card.js
@@ -36,30 +36,28 @@ class Card extends Component {
       var speciality = []
       speciality = network['speciality']
       console.log('speciality' + JSON.stringify(card))
-      cards.push(<View style={i % 2 == 0 ? styles.cardStyle : styles.cardStyle1} >
-        <Text style={styles.h1}>
+      cards.push(
+        <View style={i % 2 == 0 ? styles.cardStyle : styles.cardStyle1}>
+        <Text style={styles.h1} >
           {network.header_text.en}
         </Text>
-
         {
           speciality.map(specialities=>{
             const{speciality_text, speciality_value}=specialities
-            return <View >
-            <Text style = {styles.h4} >
+            return(
+            <View >
+            <Text style = {styles.h4}>
             {speciality_text['en']}
             </Text>
             {
               speciality_value.map(value=>{
-                return <Text style={styles.h2}>
+                return <Text style={styles.h2} >
                 {value['en']}</Text>
               })
             }
-            </View>
+           </View>)
           })
         }
-
-
-
       </View>
 
         )
