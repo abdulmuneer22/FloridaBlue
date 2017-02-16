2e41abd4bedd67e05976d542d8cc280e4460b7c1
Enugu <f7ro@mac51b5457.bcbsfl.com>
2017-02-16 16:19:00 -0500

    local changes

--- FloridaBlue/App/Services/Api.js
+++ FloridaBlue/App/Services/Api.js
@@ -3,7 +3,7 @@ import apisauce from 'apisauce'
 global.Buffer = global.Buffer || require('buffer').Buffer
 
 // our "constructor"
-const create = (baseURL = 'https://mobapi-stga.bcbsfl.com/mob/api/v1/') => {
+const create = (baseURL = 'https://mobapi-tsta.bcbsfl.com/mob/api/v1/') => {
 //const create = (baseURL = 'http://localhost:9000/mob/api/v1/') => {
   // ------
   // STEP 1
