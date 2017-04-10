25005e9814cff48396a62fa094adaa0e20ae09a1
YaswanthKumar Prathipati <yaswanth.prathipati@gmail.com>
2017-04-10 15:38:14 -0400

    updating logger changes

--- FloridaBlue/App/Redux/CreateStore.js
+++ FloridaBlue/App/Redux/CreateStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { autoRehydrate } from 'redux-persist'
-import createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import Config from '../Config/DebugSettings'
 import createSagaMiddleware from 'redux-saga'
 import R from 'ramda'
@@ -21,19 +21,20 @@ export default (rootReducer, rootSaga) => {
   middleware.push(sagaMiddleware)
 
   /* ------------- Logger Middleware ------------- */
-/*
+
   const SAGA_LOGGING_BLACKLIST = ['EFFECT_TRIGGERED', 'EFFECT_RESOLVED', 'EFFECT_REJECTED', 'persist/REHYDRATE']
   if (__DEV__) {
     // the logger master switch
     const USE_LOGGING = Config.reduxLogging
     // silence these saga-based messages
     // create the logger
+    
     const logger = createLogger({
       predicate: (getState, { type }) => USE_LOGGING && R.not(R.contains(type, SAGA_LOGGING_BLACKLIST))
     })
     middleware.push(logger)
   }
-*/
+
   /* ------------- Assemble Middleware ------------- */
 
   enhancers.push(applyMiddleware(...middleware))
