e2786471bb1bc423535bdb2acb125ec3fee94bd9
Chintha <e9kz@mac51b3994.bcbsfl.com>
2017-05-04 11:42:06 -0400

    env specific changes added

--- FloridaBlue/App/Sagas/index.js
+++ FloridaBlue/App/Sagas/index.js
@@ -62,16 +62,17 @@ import {sendStaffLanguageRequest} from './ProviderSagas'
 import {sendConfigTypeRequest} from './ProviderSagas'
 import {sendDoctorDetailRequest} from './ProviderSagas'
 // import { getTemperature } from './TemperatureSagas'
+var urlConfig = require('../UrlConfig');
 
 /* ------------- API ------------- */
 
 // The API we use is only used from Sagas, so we create it here and pass along
 // to the sagas which need it.
 const api = DebugSettings.useFixtures ? FixtureAPI : API.create()
-const apiforRegistration = API.create(baseURL = 'https://registration-stga.bcbsfl.com/ers/api-public/v1/users/')
+const apiforRegistration = API.create(baseURL = urlConfig.registrationURL)
 // const apiforSecurity = API.create(baseURL = 'https://registration-stga.bcbsfl.com/ers/api/v1/users/')
 // const apiforRegistration = API.create(baseURL = 'http://localhost:3000/api')
-const apiforlogout = API.create(baseURL = 'https://logout-stga.bcbsfl.com/')
+const apiforlogout = API.create(baseURL = urlConfig.logoutURL)
 
 /* ------------- Connect Types To Sagas ------------- */
 
