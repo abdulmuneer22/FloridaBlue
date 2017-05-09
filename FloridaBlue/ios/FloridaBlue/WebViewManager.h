fa2fe48fa60258608c669c716454d6e05f0a30b5
Wright, Brock <Brock.Wright@bcbsfl.com>
2017-05-09 14:22:13 -0400

    Added WebViewManager iOS native classes..

---
+++ FloridaBlue/ios/FloridaBlue/WebViewManager.h
@@ -0,0 +1,15 @@
+//
+//  WebViewController.h
+//  FloridaBlue
+//
+//  Created by Wright, Brock on 5/9/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YWebView.h"
+#import <React/RCTViewManager.h>
+
+@interface WebViewManager : RCTViewManager
+
+@end
