af417c3373492919bce1def51d2dcb7cdb73d38d
Enugu <anveshreddy.enugu@bcbsfl.com>
2017-05-16 18:50:13 -0400

    adjusted styles in doctor list page

--- FloridaBlue/App/Containers/MemberDashBoard/Containers/OPD/DoctorList/DoctorList.js
+++ FloridaBlue/App/Containers/MemberDashBoard/Containers/OPD/DoctorList/DoctorList.js
@@ -46,37 +46,44 @@ const theme = getTheme()
 const SingleColorSpinner = MKSpinner.singleColorSpinner()
   .withStyle(styles.spinner)
   .build()
-
+ 
 class DoctorList extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      listLimit: 10,
-      totalNumberOfCardPerScreen: 30,
-      isFetchingMore: false
-    }
-    this.loadMore = this.loadMore.bind(this)
-  }
+constructor(props){
+         super(props);
+         this.state = {
+          listLimit : 10,
+          totalNumberOfCardPerScreen : 30,
+          isFetchingMore: false,
+          loadingMore:true,
+          initialCount:0,
+          finalCount:0
+      }
+      this.loadMore = this.loadMore.bind(this)
+   }
+
 
   _advancedSearch () {
-    NavigationActions.AdvancedSearch({navigatingFrom: 'doctorsListPage'})
+    NavigationActions.AdvancedSearch({navigatingFrom: 'doctorsListPage'});
   }
 
   _mapView () {
-    if (this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0) {
-      NavigationActions.ProviderMap()
-    }
+      if(this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0){
+          NavigationActions.ProviderMap()
+      }
   }
   componentDidMount () {
-
+  
   }
 
   componentWillReceiveProps (newProps) {
-    console.tron.log(newProps)
+    console.tron.log("displaying new props****", newProps)
     if (this.state.isFetchingMore) {
-      this.props.attemptProviderSearch(newProps)
+
+    this.props.attemptProviderSearch(newProps)
       this.setState({isFetchingMore: false})
+
+     
     }
 
     if (newProps.provider && newProps.provider.data && newProps.provider.data.originLatitude != '' && newProps.provider.data.originLongitude != '') {
@@ -104,26 +111,28 @@ class DoctorList extends Component {
     </Image>)
   }
 
-  loadMore () {
-    var currentLimit = this.state.listLimit
-    console.log('currentLimit=>', currentLimit)
-    var newLimit = currentLimit
-    console.log('new limit =>', newLimit)
+    loadMore() {
+       var currentLimit = this.state.listLimit
+        console.log("currentLimit=>" , currentLimit)
+        var newLimit = currentLimit
+        console.log("new limit =>" , newLimit)
 
-    this.setState({
-      listLimit: newLimit + 10
-    })
+        this.setState({
+            listLimit : newLimit + 10
+        })
 
-    if (this.state.totalNumberOfCardPerScreen == newLimit) {
-      this.props.changeEnd(this.state.totalNumberOfCardPerScreen + 30)
-      this.state.isFetchingMore = true
+       
+            if(this.state.totalNumberOfCardPerScreen == newLimit) {
+           this.props.changeEnd(this.state.totalNumberOfCardPerScreen + 30)
+           this.state.isFetchingMore = true
 
-      this.setState({
-             // listLimit : this.state.totalNumberOfCardPerScreen,
-        totalNumberOfCardPerScreen: this.state.totalNumberOfCardPerScreen + 30
-      })
-    }
-  }
+           this.setState({
+        //      // listLimit : this.state.totalNumberOfCardPerScreen,
+               totalNumberOfCardPerScreen : this.state.totalNumberOfCardPerScreen + 30
+           })
+         }
+ 
+     }
 
   _displayCondition () {
     if (this.props.fetching) {
@@ -132,43 +141,39 @@ class DoctorList extends Component {
         <Text style={styles.spinnerText}>Loading Please Wait </Text>
       </View>)
     } else if (this.props.provider && this.props.provider.data) {
+   
       return (
         <View style={styles.container}>
           {this.props.provider ?
             <View style={{flex: 9}}>
 
               <ScrollView >
+ 
 
-                {this.props.showUrgentCareBanner ?
-                  <View style={{flex: 1, margin: 15 }}>
-                    <Card style={{flex: 1, borderRadius: 15, backgroundColor: 'purple'}} >
-                      <View style={{ flexDirection: 'row', margin: 5, alignItems: 'center', justifyContent: 'center' }}>
-                        <View style={{ flex: 0.15 }}>
-                          <Flb name='accident' size={Metrics.icons.large} color={Colors.snow} />
-                        </View>
-                        <View style={{ flex: 0.85 }}>
-                          <Text style={{
-                            fontSize: Fonts.size.input * Metrics.screenWidth * 0.0028,
-                            color: Colors.snow
-                          }}>If this is an emergency, please call 911.</Text>
-                        </View>
+              {this.props.showUrgentCareBanner ?
+                <View style={{flex: 1, margin: 15 }}>
+                  <Card style={{flex: 1, borderRadius: 15, backgroundColor:Colors.flBlue.red}} >
+                     <View style={{ flexDirection: 'row', margin: 5, alignItems: 'center', justifyContent: 'center' }}>
+                      <View style={{ flex: 0.15 }}>
+                        <Flb name='accident' size={Metrics.icons.large} color={Colors.snow} />
                       </View>
-                    </Card>
-                  </View>
+                      <View style={{ flex: 0.85 }}>
+                        <Text style={{
+                          fontSize: Fonts.size.input * Metrics.screenWidth * 0.0028,
+                          color: Colors.snow
+                        }}>If this is an emergency, please call 911.</Text>
+                      </View>
+                    </View>
+                   </Card>
+                </View>
               : null}
 
-                <View style={{flex: 1}}>
+                <View style={{flex: 1, marginTop:-20}}>
 
                   {this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0 ?
-                    <DoctorCard
-                      cardLimit={this.state.listLimit}
+                     <DoctorCard
+                      cardLimit = {this.state.listLimit}
                       data={this.props.provider.data.providerList}
-                    // savedproviders={this.props.saveProvider}
-                    // saveProvider={this.saveProvider}
-                    // removeProvider={this.removeProvider}
-                    // leftActive={this.props.leftActive}
-                    // rightActive={this.props.rightActive}
-
                   />
                   :
                       <LinearGradient style={{flex: 1, margin: 15, borderRadius: 20}} colors={['#EECDA3', '#EF629F']}>
@@ -176,23 +181,23 @@ class DoctorList extends Component {
 
                           <Card style={{flex: 1, borderRadius: 20, justifyContent: 'center'}}>
 
-                            <View style={{flex: 1, margin: 15}}>
+                           <View style={{flex: 1, margin: 15}}>
 
-                              <Text style={{fontSize: Fonts.size.h6 * Metrics.screenWidth * 0.0028,
-                                color: Colors.flBlue.anvil
-                              }}>Oops! We did not find an exact match for your search. Try a new Search.</Text>
+                            <Text style={{fontSize: Fonts.size.h6 * Metrics.screenWidth * 0.0028,
+                            color: Colors.flBlue.anvil
+                          }}>Oops! We did not find an exact match for your search. Try a new Search.</Text>
 
-                            </View>
+                          </View>
 
-                          </Card>
+                         </Card>
 
                         </View>
                       </LinearGradient>
                     }
 
                 </View>
-                {this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0 ?
-                  <View style={{flex: 1}}>
+              {this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0 ?
+                  <View style={{flex: 1,marginBottom:10}}>
                     <TouchableOpacity
                       onPress={this.loadMore}
                       style={{
@@ -211,8 +216,8 @@ class DoctorList extends Component {
                         color: 'white'
                       }}>Load More</Text>
                     </TouchableOpacity>
-                  </View> : null}
-              </ScrollView>
+                  </View> : null} 
+                </ScrollView>
             </View>
 
           : <View style={styles.spinnerView}>
@@ -226,41 +231,41 @@ class DoctorList extends Component {
                 <TouchableOpacity style={{flex: 1}} onPress={() => this._advancedSearch()}>
                   <View style={styles.refinesearch}>
 
-                    <View style={{flex: 0.3, alignItems: 'center'}}>
+                     <View style={{flex: 0.3, alignItems: 'center'}}>
                       <Flb
                         name='search-find'
                         size={Metrics.icons.medium}
                         color={Colors.snow} />
                     </View>
 
-                    <View style={{flex: 0.7, alignItems: 'flex-start'}}>
+                     <View style={{flex: 0.7, alignItems: 'flex-start'}}>
 
                       <Text style={styles.footerText}>Refine Search</Text>
                     </View>
 
-                  </View>
+                   </View>
                 </TouchableOpacity>
                 <TouchableOpacity style={
                   this.props.provider && this.props.provider.data && this.props.provider.data.providerList && this.props.provider.data.providerList.length > 0 ?
-                  {flex: 1, backgroundColor: Colors.flBlue.grass} : {flex: 1, backgroundColor: Colors.flBlue.grey3}
+                  {flex: 1, backgroundColor:Colors.flBlue.grass}:{flex: 1, backgroundColor:Colors.flBlue.grey3}
                 }
-                  onPress={() => this._mapView()}>
+                onPress={() => this._mapView()}>
                   <View style={styles.footerView}>
 
-                    <View style={{flex: 0.4, alignItems: 'center'}}>
+                     <View style={{flex: 0.4, alignItems: 'center'}}>
                       <Flb
                         name='map'
                         size={Metrics.icons.medium}
                         color={Colors.snow} />
                     </View>
 
-                    <View style={{flex: 0.6,
+                     <View style={{flex: 0.6,
                       alignItems: 'flex-start'}}>
 
                       <Text style={styles.footerText}>Map View</Text>
                     </View>
 
-                  </View>
+                   </View>
                 </TouchableOpacity>
               </View>
 
@@ -288,7 +293,7 @@ class DoctorList extends Component {
           {this._renderHeader()}
         </View>
 
-        <View style={{flex: 7}}>
+        <View style={{flex: 7,marginTop:10}}>
           {
          this._displayCondition()
        }
@@ -306,7 +311,7 @@ DoctorList.propTypes = {
   error: PropTypes.string,
   saveProvider: PropTypes.array,
   attemptHandleLeft: PropTypes.func,
-  attemptHandleRight: PropTypes.func
+  attemptHandleRight: PropTypes.func,
 
 }
 
@@ -315,16 +320,16 @@ const mapStateToProps = (state) => {
     fetching: state.provider.fetching,
     error: state.provider.error,
    // leftActive: state.provider.leftActive,
-    // rightActive: state.provider.rightActive,
-    // saveProvider: state.saveprovider.data,
+    //rightActive: state.provider.rightActive,
+    //saveProvider: state.saveprovider.data,
     provider: state.provider.data,
     latitude: state.provider.latitude,
     longitude: state.provider.longitude,
     searchRange: state.provider.searchRange,
     latDelta: state.provider.latDelta,
     longDelta: state.provider.longDelta,
-    start: state.provider.start,
-    end: state.provider.end,
+    start:state.provider.start,
+    end:state.provider.end,
     networkCodeList: state.provider.networkCodeList,
     showUrgentCareBanner: state.provider.showUrgentCareBanner,
     planCategoryList: state.provider.planCategoryList,
@@ -355,7 +360,7 @@ const mapStateToProps = (state) => {
     gender: state.provider.gender,
     programsList: state.provider.programsList,
     officeHours: state.provider.officeHours
-
+  
   }
 }
 
