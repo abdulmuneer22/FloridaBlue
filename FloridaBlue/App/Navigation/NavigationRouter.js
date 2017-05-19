6c4d1b3a5f1efd054595ba62e0155c521b09f10b
Enugu <anveshreddy.enugu@bcbsfl.com>
2017-05-19 17:30:20 -0400

    Removed Actions.REset from NAvigation

--- FloridaBlue/App/Navigation/NavigationRouter.js
+++ FloridaBlue/App/Navigation/NavigationRouter.js
@@ -52,7 +52,7 @@ class NavigationRouter extends Component {
       <Router>
             <Scene key='drawer' component={NavigationDrawer} open={false}>
             <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
-            <Scene initial key='login' component={Login} title='Login' hideNavBar panHandlers={null} type={ActionConst.RESET} />
+            <Scene initial key='login' component={Login} title='Login' hideNavBar panHandlers={null}/>
             <Scene key='TouchTOU' component={TouchTOU} title="Touch TOU" hideNavBar />
             <Scene key='Termsofuse' component={TermsofUse} title='Termsofuse page' hideNavBar />
             <Scene key='MyView' component={Webview} title='WebView page' hideNavBar />
@@ -67,7 +67,7 @@ class NavigationRouter extends Component {
             <Scene key='useridhint' component={UserIdHint} title='UserIdHint page' hideNavBar />
             <Scene key='passwordHint' component={PasswordHint} title='PasswordHint page' hideNavBar />
             <Scene key='securityHint' component={SecurityHint} title='SecurityHint page' hideNavBar />
-            <Scene key='WelcomeDashBoard' component={DashBoard} title='Florida Blue' hideNavBar type={ActionConst.RESET}/>
+            <Scene key='WelcomeDashBoard' component={DashBoard} title='Florida Blue' hideNavBar />
             <Scene key='Resources' component={Resources} title='Resource WebView' hideNavBar />
             <Scene key='Hsa' component={HSA} title='Health Savings Account Page' hideNavBar />
             <Scene key='SupportScreen' component={SupportScreen} title='Support Page' hideNavBar />
