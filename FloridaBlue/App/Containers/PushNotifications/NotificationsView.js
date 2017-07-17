08e530b05c7bef343e53fabaa848328b348b0170
Prathipati <f7ih@mac71b3290.bcbsfl.com>
2017-07-16 21:21:48 -0400

    Notification changes

--- FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
+++ FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
@@ -24,9 +24,10 @@ import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import PushController from './PushController'
 import FCM, { FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType } from 'react-native-fcm'
-
+import Swipeout from 'react-native-swipeout'
 import NotificationActions from '../../Redux/NotificationRedux'
 const theme = getTheme()
+import rows from './data'
 
 const SingleColorSpinner = MKSpinner.singleColorSpinner()
   .withStyle(styles.spinner)
@@ -35,10 +36,15 @@ const SingleColorSpinner = MKSpinner.singleColorSpinner()
 class NotificationsView extends Component {
   constructor (props) {
     super(props)
+    this.deleteRow = this.deleteRow.bind(this)
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
     this.state = {
       basic: true,
-      listViewData: {}
+      listViewData: {},
+      secId: null,
+      rowId: null,
+      messageId: null
+
     }
   }
 
@@ -63,15 +69,17 @@ class NotificationsView extends Component {
     })
   }
 
-  deleteRow (secId, rowId, rowMap, messageId) {
-    console.log(secId, rowId, rowMap, messageId)
-    rowMap[`${secId}${rowId}`].closeRow()
+  deleteRow (secId, rowId) {
+    console.log(secId, rowId)
+   // rowMap[`${secId}${rowId}`].closeRow()
     const newData = [...this.props.notification]
     newData.splice(rowId, 1)
+    /*
     this.props.postArchive({
       'messageId': messageId,
       'markAllRead': false
     })
+    */
     console.log('newData', newData)
     if (newData) {
       this.props.deleteNotification(newData)
@@ -93,58 +101,98 @@ class NotificationsView extends Component {
         <Text style={styles.spinnerText}>Loading Please Wait </Text>
       </View>)
     } else if (this.props.notification && Object(this.props.notification).length > 0) {
+      var that = this
       return (
-        <SwipeListView style={{ marginTop: 10, margin: 10, flex: 1 }}
+        <ListView style={{flex: 1}}
+          scrollEnabled
           dataSource={this.props.notification && Object(this.props.notification).length > 0 ? this.ds.cloneWithRows(this.props.notification) : {}}
           enableEmptySections
           disableRightSwipe
           renderRow={(data, secId, rowId, rowMap) => (
-            <View style={styles.rowFront}>
-              <View style={{ flex: 1, marginTop: Metrics.baseMargin*Metrics.screenHeight*0.002 }}>
-                <View style={{ flexDirection: 'row', marginLeft: Metrics.doubleBaseMargin*Metrics.screenWidth*0.002, marginTop: Metrics.baseMargin*Metrics.screenHeight*0.002, alignItems: 'center', flex: 1 }}>
-                  <Text allowFontScaling={false} style={{
-                    color: Colors.flBlue.anvil,
-                    marginLeft: 10,
-                    fontSize: Fonts.size.regular * Metrics.screenWidth * 0.002
-                  }}>
-                    {data.sendDate}
-                  </Text>
-                </View>
-                <View style={{ flex: 1 }}>
-                  <View style={{
-                    paddingTop: Metrics.baseMargin*Metrics.screenHeight*0.002,
-                    flexDirection: 'row',
-                    marginRight: Metrics.baseMargin*Metrics.screenWidth*0.001,
-                    flex: 1
-                  }}>
-                    <View style={{ marginLeft: Metrics.baseMargin*Metrics.screenWidth*0.002, paddingTop: 5, flex: 0.2 }}>
-                      <Flb name={data.style.icon} size={Metrics.icons.large * Metrics.screenWidth * 0.0025} color={Colors.flBlue[data.style.color]} />
-                    </View>
-                    <View style={{ flex: 0.8, marginRight: 10 }}>
-                      <View style={{ flex: 0.4 }}>
-                        <Text allowFontScaling={false} style={{
-                          fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0025,
-                          color: Colors.flBlue.anvil,
-                          fontWeight: '500',
-                          marginBottom: 1,
-                          paddingTop: 5,
-                          marginLeft: 1,
-                          marginRight: Metrics.doubleBaseMargin*Metrics.screenWidth*0.002
-                        }}>{data.title}</Text>
-                      </View>
-                      <View style={{ flex: 0.4, marginTop: Metrics.baseMargin*Metrics.screenHeight*0.002 }}>
-                        <Text style={{
-                          marginBottom: 2,
-                          color: Colors.flBlue.anvil,
-                          fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0022
-                        }} >
-                          {data.cardContent}
-                        </Text>
-                      </View>
-                    </View>
-                  </View>
 
-                 
+            <Swipeout
+              style={{
+                margin: 10
+              }}
+              close={!(this.state.secId === secId && this.state.rowId === rowId)}
+              right={
+                [{
+                  text: 'Press Me',
+                  onPress: function (rowId, secId) {
+                    const newData = [...that.props.notification]
+                    console.log('inside newDate', newData)
+                    console.log('inside secId, rowId', that.state.secId, that.state.rowId)
+                    console.log('inside data', that.props.notification[that.state.rowId].messageId)
+                    newData.splice(that.state.rowId, 1)
+                    that.props.postArchive({
+                      'messageId': that.props.notification[that.state.rowId].messageId,
+                      'markAllRead': false
+                    })
+                    if (newData) {
+                      that.props.deleteNotification(newData)
+                    }
+                  },
+                  type: 'secondary'
+                }]}
+              rowID={rowId}
+              sectionID={secId}
+              messageId={data.messageId}
+              autoClose
+              backgroundColor={Colors.bg2}
+              onOpen={(secId, rowId) => {
+                this.setState({
+                  secId,
+                  rowId
+                })
+              }}
+              onClose={() => console.log('===close')}
+              scroll={event => console.log('scroll event')}
+      >
+              <View style={styles.rowFront}>
+                <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: 'row', marginLeft: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002, marginTop: Metrics.baseMargin * Metrics.screenHeight * 0.002, alignItems: 'center', flex: 1 }}>
+                    <Text allowFontScaling={false} style={{
+                      color: Colors.flBlue.anvil,
+                      marginLeft: 10,
+                      fontSize: Fonts.size.regular * Metrics.screenWidth * 0.002
+                    }}>
+                      {data.sendDate}
+                    </Text>
+                  </View>
+                  <View style={{ flex: 1 }}>
+                    <View style={{
+                      paddingTop: Metrics.baseMargin * Metrics.screenHeight * 0.002,
+                      flexDirection: 'row',
+                      marginRight: Metrics.baseMargin * Metrics.screenWidth * 0.001,
+                      flex: 1
+                    }}>
+                      <View style={{ marginLeft: Metrics.baseMargin * Metrics.screenWidth * 0.002, paddingTop: 5, flex: 0.2 }}>
+                        <Flb name={data.style.icon} size={Metrics.icons.large * Metrics.screenWidth * 0.0025} color={Colors.flBlue[data.style.color]} />
+                      </View>
+                      <View style={{ flex: 0.8, marginRight: 10 }}>
+                        <View style={{ flex: 0.4 }}>
+                          <Text allowFontScaling={false} style={{
+                            fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0025,
+                            color: Colors.flBlue.anvil,
+                            fontWeight: '500',
+                            marginBottom: 1,
+                            paddingTop: 5,
+                            marginLeft: 1,
+                            marginRight: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002
+                          }}>{data.title}</Text>
+                        </View>
+                        <View style={{ flex: 0.4, marginTop: Metrics.baseMargin * Metrics.screenHeight * 0.002 }}>
+                          <Text style={{
+                            marginBottom: 2,
+                            color: Colors.flBlue.anvil,
+                            fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0022
+                          }} >
+                            {data.cardContent}
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+
                     <TouchableOpacity onPress={() => {
                       if (data.link) {
                         if (data.link.type === 'webview') {
@@ -160,77 +208,48 @@ class NotificationsView extends Component {
                     }}
                       style={{
                         flex: 1,
-                    borderBottomLeftRadius: Metrics.baseMargin*Metrics.screenWidth*0.002,
-                    borderBottomRightRadius: Metrics.baseMargin*Metrics.screenWidth*0.002,
-                    justifyContent: 'center',
+                        borderBottomLeftRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        borderBottomRightRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        justifyContent: 'center',
                    // alignItems:'center',
-                    marginTop: Metrics.smallMargin*Metrics.screenWidth*0.002,
+                        marginTop: Metrics.smallMargin * Metrics.screenWidth * 0.002,
                       // marginBottom:10,
-                   backgroundColor: Colors.flBlue[data.style.barColor]
+                        backgroundColor: Colors.flBlue[data.style.barColor]
                       }}>
-                       <View style={{
-                    flex: 1,
-                    borderBottomLeftRadius: Metrics.baseMargin*Metrics.screenWidth*0.002,
-                    borderBottomRightRadius: Metrics.baseMargin*Metrics.screenWidth*0.002,
-                    justifyContent: 'center',
+                      <View style={{
+                        flex: 1,
+                        borderBottomLeftRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        borderBottomRightRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        justifyContent: 'center',
                    // alignItems:'center',
-                    marginTop: Metrics.smallMargin*Metrics.screenWidth*0.002,
+                        marginTop: Metrics.smallMargin * Metrics.screenWidth * 0.002,
                       // marginBottom:10,
-                    backgroundColor: Colors.flBlue[data.style.barColor]
-                  }}>
-                      <View style={{ flex: 1, marginTop: 10 }}>
-                        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-                          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                            <Text allowFontScaling={false} style={{
-                              color: Colors.flBlue[data.style.color],
-                              marginLeft: Metrics.doubleBaseMargin*Metrics.screenWidth*0.002,
-                              fontSize: Fonts.size.regular,
-                              marginBottom:Metrics.smallMargin*Metrics.screenWidth*0.002
-                            }}>{data && data.link ? data.link.desc : null }</Text>
-                            {data && data.link
-                            ? <Flb name='chevron-right' style={{marginBottom:Metrics.smallMargin*Metrics.screenWidth*0.002, marginLeft:Metrics.smallMargin*Metrics.screenWidth*0.003, color: Colors.flBlue[data.style.color] }}
+                        backgroundColor: Colors.flBlue[data.style.barColor]
+                      }}>
+                        <View style={{ flex: 1, marginTop: 10 }}>
+                          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                              <Text allowFontScaling={false} style={{
+                                color: Colors.flBlue[data.style.color],
+                                marginLeft: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002,
+                                fontSize: Fonts.size.regular,
+                                marginBottom: Metrics.smallMargin * Metrics.screenWidth * 0.002
+                              }}>{data && data.link ? data.link.desc : null }</Text>
+                              {data && data.link
+                            ? <Flb name='chevron-right' style={{marginBottom: Metrics.smallMargin * Metrics.screenWidth * 0.002, marginLeft: Metrics.smallMargin * Metrics.screenWidth * 0.003, color: Colors.flBlue[data.style.color] }}
                               size={Metrics.icons.small * Metrics.screenWidth * 0.002} /> : <View />}
+                            </View>
                           </View>
                         </View>
                       </View>
-                       </View>
                     </TouchableOpacity>
-                 
+
+                  </View>
                 </View>
               </View>
-            </View>
+            </Swipeout>
             )}
-          renderHiddenRow={(data, secId, rowId, rowMap) => (
-            <View style={{
-              alignItems: 'center',
-              flex: 1,
-              marginTop: 5,
-              flexDirection: 'row',
-              borderRadius: 20
-            }}>
-              <TouchableOpacity style={{
-                alignItems: 'flex-end',
-                justifyContent: 'center',
-                flex: 1,
-                borderRadius: 20,
-                backgroundColor: 'green',
-                right: 0
-              }} onPress={_ => this.deleteRow(secId, rowId, rowMap, data.messageId)} >
-                <View style={{
-                  flex: 1,
-                  alignItems: 'flex-end',
-                    // bottom: 0,
-                  justifyContent: 'center'
-                }}>
-                  <Text allowFontScaling={false} style={{
-                    color: Colors.snow,
-                    marginRight: 30,
-                    fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0025
-                  }}>Clear</Text >
-                </View>
-              </TouchableOpacity>
-            </View>)}
-          rightOpenValue={-90}
+
           />)
     } else if (this.props.error != null) {
       Alert.alert(
