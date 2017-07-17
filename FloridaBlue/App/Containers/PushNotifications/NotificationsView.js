e98a11b63c8e2891778335b3e058203df520793c
Prathipati <f7ih@mac71b3290.bcbsfl.com>
2017-07-16 23:21:24 -0400

    adding archive instead of clear

--- FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
+++ FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
@@ -120,7 +120,7 @@ class NotificationsView extends Component {
               close={!(this.state.secId === secId && this.state.rowId === rowId)}
               right={
                 [{
-                  text: 'Clear',
+                  text: 'Archive',
                   onPress: function () {
                     const newData = [...that.props.notification]
                     newData.splice(that.state.rowId, 1)
