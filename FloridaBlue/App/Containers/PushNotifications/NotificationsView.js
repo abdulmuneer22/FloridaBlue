dccb474513df2d10c66ad46c38c9fd927ce459ce
Prathipati <f7ih@mac71b3290.bcbsfl.com>
2017-07-19 16:19:17 -0400

    fixing feedbak for testing

--- FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
+++ FloridaBlue/App/Containers/PushNotifications/NotificationsView.js
@@ -113,7 +113,114 @@ class NotificationsView extends Component {
           disableRightSwipe
           renderRow={(data, secId, rowId, rowMap) => (
 
-            <Swipeout
+            data.style.icon === 'exclamation-circle'
+            ? <View
+              style={{
+                margin: 10
+              }}>
+              <View style={styles.rowFront}>
+                <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: 'row', marginLeft: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002, marginTop: Metrics.baseMargin * Metrics.screenHeight * 0.002, alignItems: 'center', flex: 1 }}>
+                    <Text allowFontScaling={false} style={{
+                      color: Colors.flBlue.anvil,
+                      fontSize: Fonts.size.regular * Metrics.screenWidth * 0.002
+                    }}>
+                      {data.sendDate}
+                    </Text>
+                  </View>
+                  <View style={{ flex: 1 }}>
+                    <View style={{
+                      paddingTop: Metrics.baseMargin * Metrics.screenHeight * 0.002,
+                      flexDirection: 'row',
+                      marginRight: Metrics.baseMargin * Metrics.screenWidth * 0.001,
+                      flex: 1
+                    }}>
+                      <View style={{ marginLeft: Metrics.baseMargin * Metrics.screenWidth * 0.002, paddingTop: 5, flex: 0.2 }}>
+                        <Flb name={data.style.icon} size={Metrics.icons.large * Metrics.screenWidth * 0.0025} color={Colors.flBlue[data.style.color]} />
+                      </View>
+                      <View style={{ flex: 0.8, marginRight: 10 }}>
+                        <View style={{ flex: 0.4 }}>
+                          <Text allowFontScaling={false} style={{
+                            fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0025,
+                            color: Colors.flBlue.anvil,
+                            fontWeight: '500',
+                            marginBottom: 1,
+                            paddingTop: 5,
+                            marginLeft: 1,
+                            marginRight: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002
+                          }}>{data.title}</Text>
+                        </View>
+                        <View style={{ flex: 0.4, marginTop: Metrics.baseMargin * Metrics.screenHeight * 0.002, marginBottom: 12}}>
+                          <Text style={{
+                            marginBottom: 2,
+                            color: Colors.flBlue.anvil,
+                            fontSize: Fonts.size.regular * Metrics.screenWidth * 0.0022
+                          }} >
+                            {data.cardContent}
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+
+                    <TouchableOpacity onPress={() => {
+                      if (data.link) {
+                        if (data.link.type === 'webview') {
+                          var webview = 'MyView'
+                          NavigationActions[webview]({ responseURL: data.link.name })
+                        } else if (data.link.type === 'native') {
+                          var routerName = data.link.name
+                          NavigationActions[routerName]()
+                        } else if (data.link.type === 'href') {
+                          Linking.openURL(data.link.name)
+                        }
+                      }
+                    }}
+                      style={{
+                        flex: 1,
+                        borderBottomLeftRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        borderBottomRightRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        justifyContent: 'center',
+                   // alignItems:'center',
+                        marginTop: Metrics.smallMargin * Metrics.screenWidth * 0.002,
+                      // marginBottom:10,
+                        backgroundColor: Colors.flBlue[data.style.barColor]
+                      }}>
+                      <View style={{
+                        flex: 1,
+                        borderBottomLeftRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        borderBottomRightRadius: Metrics.baseMargin * Metrics.screenWidth * 0.002,
+                        justifyContent: 'center',
+                   // alignItems:'center',
+                        marginTop: Metrics.smallMargin * Metrics.screenWidth * 0.002,
+                      // marginBottom:10,
+                        backgroundColor: Colors.flBlue[data.style.barColor]
+                      }}>
+                        <View style={{ flex: 1, margin: 5 }}>
+                          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                              <Text allowFontScaling={false} style={{
+                                color: Colors.flBlue[data.style.color],
+                                marginLeft: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002,
+                                fontSize: Fonts.size.xm,
+                                marginBottom: Metrics.smallMargin * Metrics.screenWidth * 0.002
+                              }}>{data && data.link ? data.link.desc : null }</Text>
+                              {data && data.link
+                            ? <Flb name='chevron-right' style={{
+                            //  marginBottom: Metrics.smallMargin * Metrics.screenWidth * 0.002,
+                              marginLeft: Metrics.smallMargin * Metrics.screenWidth * 0.003,
+                              color: Colors.flBlue[data.style.color] }}
+                              size={Metrics.icons.small * Metrics.screenWidth * 0.002} /> : <View />}
+                            </View>
+                          </View>
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+
+                  </View>
+                </View>
+              </View>
+            </View>
+            : <Swipeout
               style={{
                 margin: 10
               }}
@@ -149,7 +256,7 @@ class NotificationsView extends Component {
               }}
               onClose={() => console.log('===close')}
               scroll={event => console.log('scroll event', event)}
-      >
+              >
               <View style={styles.rowFront}>
                 <View style={{ flex: 1 }}>
                   <View style={{ flexDirection: 'row', marginLeft: Metrics.doubleBaseMargin * Metrics.screenWidth * 0.002, marginTop: Metrics.baseMargin * Metrics.screenHeight * 0.002, alignItems: 'center', flex: 1 }}>
@@ -252,7 +359,8 @@ class NotificationsView extends Component {
                 </View>
               </View>
             </Swipeout>
-            )}
+
+          )}
 
           />)
     } else if (this.props.error != null) {
