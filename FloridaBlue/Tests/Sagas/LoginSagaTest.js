af8f2fdce711f3ef06586cb0a47e9385c376e234
Chintha <e9kz@mac51b3994.bcbsfl.com>
2017-07-19 21:54:36 -0400

    removed user id and password

--- FloridaBlue/Tests/Sagas/LoginSagaTest.js
+++ FloridaBlue/Tests/Sagas/LoginSagaTest.js
@@ -6,7 +6,7 @@ import LoginActions from '../../App/Redux/LoginRedux'
 const stepper = (fn) => (mock) => fn.next(mock).value
 
 test('success', (t) => {
-  const mock = { username: 'a', password: 'b' }
+  const mock = {}
   const step = stepper(login(mock))
 
   t.deepEqual(step(), put(LoginActions.loginSuccess(mock.username)))
