d5d5745006865eec275188f68d45c1ce02b6f1ca
Schwartz <g6zm@mac41b6608.bcbsfl.com>
2017-08-07 11:40:01 -0400

    added tablet header image for benefits & webview; refactored locking mechanism on ID Card

--- FloridaBlue/App/Containers/WebView/Webview.js
+++ FloridaBlue/App/Containers/WebView/Webview.js
@@ -26,11 +26,11 @@ var WEBVIEW_REF = 'webview'
 
 class Webview extends Component {
   _renderHeader () {
-    return (<Image style={this.props.isPortrait ? styles.headerContainer : styles.headerContainerLandscape} source={this.props.isPortrait ? DeviceInfo.isTablet() ? Images.landscapeHeaderImage : Images.newHeaderImage : Images.landscapeHeaderImage}>
+    return (<Image style={[styles.headerContainer, {width: DeviceInfo.isTablet() ? (this.props.isPortrait ? Metrics.screenWidth : Metrics.screenWidth * 1.335) : (this.props.isPortrait ? Metrics.screenHeight : Metrics.screenWidth * 1.78)}]} source={this.props.isPortrait ? DeviceInfo.isTablet() ? Images.landscapeHeaderImage : Images.newHeaderImage : Images.landscapeHeaderImage}>
       <View style={{marginLeft: Metrics.baseMargin * Metrics.screenWidth * 0.0010}}>
         {NavItems.backButton()}
       </View>
-      <View style={this.props.isPortrait ? styles.headerView : styles.headerViewLandscape}>
+      <View style={this.props.isPortrait ? styles.headerView : [styles.headerViewLandscape, {marginRight: DeviceInfo.isTablet() ?  Metrics.images.xm * Metrics.screenWidth * 0.0065 :  Metrics.images.xm * Metrics.screenWidth * 0.011}]}>
         <Image source={Images.themeLogo} style={{
           width: Metrics.screenWidth * 0.65,
           resizeMode: 'contain',
